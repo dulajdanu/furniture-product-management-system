aa2e3b29200f7fd100d67b0f6765a4e45e1e4082
dulaj dhanushanka <dulajdhanushanka@gmail.com>
2020-06-02 01:37:39 +0530

    ongoing projects

--- src/app/user/ongoing-orders/ongoing-orders.component.ts
+++ src/app/user/ongoing-orders/ongoing-orders.component.ts
@@ -2,6 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { NbMenuItem, NbSidebarService, NbToastrService } from '@nebular/theme';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+
+
+export interface Appointment {
+  status: number;
+  descriptionOfOrder: string;
+  methodOfContact: number;
+  date: string;
+  dateAdded: Date;
+  address: string;
+  checkTypes: Array<string>;
+  dateFortheAppointment: string;
+  timeFortheAppointment: string
+}
+
+export interface AppointmentId extends Appointment {
+  id: string;
+}
 
 @Component({
   selector: 'ngx-ongoing-orders',
@@ -9,8 +27,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./ongoing-orders.component.scss']
 })
 export class OngoingOrdersComponent implements OnInit {
+  numberOfOrders: number = 0;
+  showRequestForm: boolean = false;
+  showCloseicon: boolean = false;
 
-  constructor(private sidebarService: NbSidebarService, private authService: AuthService, private router: Router, private toastrService: NbToastrService) { }
+  is_there_ongoing_appointments: boolean = false; //to find whethere there are pending appointmnets
+  ongoingAppointments: Array<any> = []; //this array is used to store the active appointments
+  no_of_ongoing: number = 0;
+
+  constructor(private sidebarService: NbSidebarService, private authService: AuthService, private router: Router, private toastrService: NbToastrService, private userService: UserService) {
+    this.userService.getAppointments().subscribe(res => {
+      // console.log(res);
+      // console.log('inside subscribe');
+      if (res.length == 0) {
+        this.numberOfOrders = 0;
+      }
+      else {
+        this.ongoingAppointments = []; //clear the arrays before adding elements
+        this.is_there_ongoing_appointments = false;
+        this.numberOfOrders = res.length;
+        res.forEach(element => {
+          // console.log(element);
+
+
+          if (element['status'] == 3) {
+            this.ongoingAppointments.push(element); //if there is a active appointment push it to the active appointments array
+          }
+        });
+        if (this.ongoingAppointments.length != 0) {
+          this.is_there_ongoing_appointments = true; //checking the pending appointments array is empty or not
+          this.no_of_ongoing = this.ongoingAppointments.length;
+        }
+
+      }
+
+      console.log(this.ongoingAppointments);
+    });
+  }
 
   load() {
     console.log('avatar clicled');
