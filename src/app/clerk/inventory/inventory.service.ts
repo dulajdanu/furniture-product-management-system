a98faf8a02f2888ed52596127f5d96b7e865b152
dulaj dhanushanka <dulajdhanushanka@gmail.com>
2020-06-10 02:28:21 +0530

    inventory report

--- src/app/clerk/inventory/inventory.service.ts
+++ src/app/clerk/inventory/inventory.service.ts
@@ -3,8 +3,8 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { NbComponentStatus, NbToastrService } from '@nebular/theme';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { firestore } from 'firebase';
+import { DatePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +13,13 @@ export class InventoryService {
 
   private itemCollection: AngularFirestoreCollection<Item>; //reference to the appointment document collection
   items: Observable<Item[]>;
+  dateToday = Date.now();
+  dateTodayString: string;
 
-  constructor(private afs: AngularFirestore, private toastrService: NbToastrService) { }
+  constructor(private afs: AngularFirestore, private toastrService: NbToastrService, private datePipe: DatePipe) {
+    this.dateTodayString = datePipe.transform(Date.now(), 'yyyyMM');
+    console.log(this.dateTodayString);
+  }
 
   showToast(status: NbComponentStatus, message: string) { //function used to show toast messages
     this.toastrService.show(status, message, { status });
@@ -24,12 +29,22 @@ export class InventoryService {
 
   addItem(val) {
     this.afs.collection("inventory").doc(val['id']).set(val).then(res => {
+
+    }).catch(res => {
+      this.showToast('danger', res);
+
+    });
+    this.afs.collection('reports').doc('InventoryReport').set({}, { merge: true });
+    this.afs.collection('reports').doc('InventoryReport').collection(this.dateTodayString).add(val).then(res => {
       this.showToast('success', "Item added successfully");
 
     }).catch(res => {
       this.showToast('danger', res);
 
     });
+
+
+
   }
 
   editItem(val) {
